fe6ea7ae0710b38536f39de2ceb65b9fb2559005
Nintoryan <thehappystudio4@gmail.com>
2021-09-01 14:05:18 +0300

    Drawing in rect. Resize wheels

--- Assets/YTaxi/Scripts/Drawing.cs
+++ Assets/YTaxi/Scripts/Drawing.cs
@@ -8,6 +8,8 @@ namespace YTaxi.Drawing
     public class Drawing : MonoBehaviour
     {
         [SerializeField] private float newPointDistance;
+        [SerializeField] private RectTransform _field;
+        [SerializeField] private float defaultLineThickness = 20f;
         [SerializeField] private UILineRenderer _uiLineRenderer;
         [SerializeField] private HoldAndDrag _holdAndDrag;
         [SerializeField] private List<Vector3> brokenLinePoints = new List<Vector3>();
@@ -15,9 +17,15 @@ namespace YTaxi.Drawing
         [SerializeField] private WheelMeshCreator _wheelMeshCreator;
         
         [SerializeField] private RectTransform m_CanvasTransform;
+        private Rect CheckingRect;
     
         private void Start()
         {
+            newPointDistance = 20f / (900f / Screen.width);
+            _uiLineRenderer.lineThickness = defaultLineThickness / (900f / Screen.width);
+            CheckingRect = _field.rect;
+            CheckingRect.center = new Vector2(Screen.width * 0.5f, Screen.height * _field.anchorMax.y *0.5f);
+
             _holdAndDrag.Started += () =>
             {
                 brokenLinePoints.Add(_holdAndDrag.CurrentPoint);
@@ -28,6 +36,7 @@ namespace YTaxi.Drawing
             {
                 if (Vector3.Distance(_holdAndDrag.CurrentPoint, brokenLinePoints[brokenLinePoints.Count-1]) > newPointDistance)
                 {
+                    if (!CheckingRect.Contains(_holdAndDrag.CurrentPoint)) return;
                     brokenLinePoints.Add(_holdAndDrag.CurrentPoint);
                     uiLinePoints.Add(_holdAndDrag.CurrentPoint);
                     _uiLineRenderer.Points = uiLinePoints.ToArray();
@@ -35,6 +44,7 @@ namespace YTaxi.Drawing
             };
             _holdAndDrag.Stopped += () =>
             {
+                ResizePoints();
                 _wheelMeshCreator.CreateWheel(brokenLinePoints);
                 brokenLinePoints.Clear();
                 uiLinePoints.Clear();
@@ -43,10 +53,18 @@ namespace YTaxi.Drawing
                 _uiLineRenderer.gameObject.SetActive(true);
             };
         }
-    
+
+        private void ResizePoints()
+        {
+            var resizingCoef = Screen.width;
+            for (int i = 0; i < brokenLinePoints.Count;i++)
+            {
+                brokenLinePoints[i] *= 500f / resizingCoef;
+            }
+        }
         private void OnDrawGizmos()
         {
-            Gizmos.matrix = m_CanvasTransform.localToWorldMatrix;
+            Gizmos.matrix = _field.localToWorldMatrix;
             var arrayOfPoints = brokenLinePoints.ToArray();
             for (int i = 1; i < brokenLinePoints.Count; i++)
             {
